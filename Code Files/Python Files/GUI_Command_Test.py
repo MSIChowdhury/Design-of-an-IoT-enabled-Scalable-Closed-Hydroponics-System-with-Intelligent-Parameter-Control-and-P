1bd09aab41dfd03dabe2b29895baefe96514f51b
MD. Sameer Iqbal Chowdhury <sameer.iqbal2171@gmail.com>
2023-11-07 01:06:25 +0600

    Update GUI_Command_Test.py

--- Code Files/Python Files/GUI_Command_Test.py
+++ Code Files/Python Files/GUI_Command_Test.py
@@ -77,7 +77,7 @@ def update_variables():
         CO2_max = 5000
     elif selected_option == 'option3':
         # Update the 14 variables based on option 3
-        ppH_min = 5.6
+        pH_min = 5.6
         pH_max = 6.0
         EC_min = 11
         EC_max = 1700
@@ -100,21 +100,21 @@ def send_data_periodically():
         try:
             # Generate random values for the 7 variables
             pH = generate_random_value(pH_min, pH_max)
-            EC = generate_random_value(EC_min, EC_max)
+            Electrical_Conductivity = generate_random_value(EC_min, EC_max)
             A_Temp = generate_random_value(A_Temp_min, A_Temp_max)
             W_Temp = generate_random_value(W_Temp_min, W_Temp_max)
             Humidity = generate_random_value(Humidity_min, Humidity_max)
             Water_Level = generate_random_value(Water_Level_min, Water_Level_max)
-            CO2 = generate_random_value(CO2_min, CO2_max)
+            CO2_gas = generate_random_value(CO2_min, CO2_max)
 
             # Replace these with actual sensor readings for each of your 7 fields
             sensor1_value = pH  # Read from your first sensor
-            sensor2_value = EC  # Read from your second sensor
+            sensor2_value = Electrical_Conductivity  # Read from your second sensor
             sensor3_value = A_Temp  # Read from your third sensor
             sensor4_value = W_Temp  # Read from your fourth sensor
             sensor5_value = Humidity  # Read from your fifth sensor
             sensor6_value = Water_Level  # Read from your sixth sensor
-            sensor7_value = CO2  # Read from your seventh sensor
+            sensor7_value = CO2_gas  # Read from your seventh sensor
 
             sensor_data = {
                 'field1': sensor1_value,
@@ -128,7 +128,7 @@ def send_data_periodically():
 
             channel = thingspeak.Channel(id=channel_id, api_key=write_key)
             response = channel.update(sensor_data)
-            print(f"pH: {pH}\nEC: {EC}\nA_Temp: {A_Temp}\nW_Temp: {W_Temp}\nHumidity: {Humidity}\nWater_Level: {Water_Level}\nCO2: {CO2}\n")
+            print(f"pH: {pH}\nEC: {Electrical_Conductivity}\nA_Temp: {A_Temp}\nW_Temp: {W_Temp}\nHumidity: {Humidity}\nWater_Level: {Water_Level}\nCO2: {CO2_gas}\n")
 
         except Exception as e:
             print(f'An error occurred: {str(e)}')
