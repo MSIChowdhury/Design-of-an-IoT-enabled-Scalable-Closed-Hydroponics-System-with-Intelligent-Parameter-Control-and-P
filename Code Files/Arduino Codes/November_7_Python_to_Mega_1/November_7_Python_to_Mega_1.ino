f1f2fc8c1ac3eec28119aab428d78d1ff47b72c1
Ronoue <warrickronoue310899@gmail.com>
2023-11-08 13:48:41 +0600

    Hola new update

---
+++ Code Files/Arduino Codes/November_7_Python_to_Mega_1/November_7_Python_to_Mega_1.ino
@@ -0,0 +1,101 @@
+#include <Arduino.h>
+#include "MHZ19.h" 
+
+//Air Temperature and Humidity Sensor
+#include "DHT.h"
+#define DHTPIN 5
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+MHZ19 myMHZ19;                                             // Constructor for library
+
+unsigned long getDataTimer = 0;
+int CO2;
+
+//Write on Mega from Python
+const int bufferSize = 4; // Adjust this according to your needs
+char inputString[bufferSize]; // A character array to store the incoming string
+char *token; // A pointer to help with parsing
+int pinNumber, pinState;
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  
+  pinMode(50,OUTPUT);
+  pinMode(38,OUTPUT);
+  
+  digitalWrite(50, HIGH);
+  digitalWrite(38, HIGH); 
+
+  //Air Temperature and Humidity Sensor
+  dht.begin();
+   
+  myMHZ19.begin(Serial1);                                // *Serial(Stream) refence must be passed to library begin(). 
+  myMHZ19.autoCalibration(false);                              // Turn auto calibration ON (OFF autoCalibration(false))
+  delay(1000);
+    
+  for(int j=0; j<=5; j++){
+    CO2 = myMHZ19.getCO2();
+    delay(100);
+  } 
+}
+
+void loop() {
+  
+    if (millis() - getDataTimer >= 1100)
+    {
+        CO2 = myMHZ19.getCO2();                       
+   
+        //This Section measures air temperature and humidity start
+        float humidity = dht.readHumidity();
+        // Read temperature as Celsius (the default)
+        float air_temperature = dht.readTemperature();
+
+        getDataTimer = millis();
+
+//      Serial.print("Humidity: ");
+//      Serial.println(humidity);
+//      Serial.print("Air Temperature: ");
+//      Serial.print(air_temperature);
+//      Serial.println(" C");      
+
+        Serial.print(CO2);
+        Serial.print(",");
+
+        Serial.print(air_temperature);
+        Serial.print(",");
+          
+        Serial.print(humidity);
+        Serial.println();
+
+    }
+  
+  if(Serial.available()!=0){
+    Serial.readBytesUntil('\n', inputString, bufferSize);
+  
+  // Parse the inputString using strtok to split it into tokens
+  token = strtok(inputString, ",");
+    
+  // Check if the token is not null (indicating that it found a comma)
+  if (token != NULL) {
+    // Convert the first token (pin number) to an integer
+    pinNumber = atoi(token);
+      
+    // Move to the next token
+    token = strtok(NULL, ",");
+      
+    // Check if the second token is not null (indicating that it found another comma)
+    if (token != NULL) {
+      // Convert the second token (pin state) to an integer
+      pinState = atoi(token);
+        
+      // Now, you have pinNumber and pinState as separate variables
+      // You can use them in your Arduino code as needed
+      // For example, you can digitalWrite(pinNumber, pinState) to set the pin stat
+      digitalWrite(pinNumber, pinState);
+      }
+    }
+  }
+  
+}
--- Code Files/Python Files/Main_Rasberry_Pi_Code_3.py
+++ Code Files/Python Files/Main_Rasberry_Pi_Code_3.py
@@ -1,14 +1,12 @@
-import gspread
 import serial
 import time
 from serial.tools import list_ports
-from oauth2client.service_account import ServiceAccountCredentials
 import datetime
 import thingspeak
 import math
-import random
 
-#All Actuator Pin Numbers
+
+# All Actuator Pin Numbers
 distilled_water_motor = "22"
 
 acid_motor = "46"
@@ -21,42 +19,25 @@ water_temperature_motor = "34"
 light_switch = "36"
 CO2_relay = "38"
 
-
-#On Off Conditions
+# On Off Conditions
 ON = ",0"
 OFF = ",1"
 
-#All pH Flags
+# All pH Flags
 pHCheck = True
-pHCheckInterval = 120.0 #Seconds
+pHCheckInterval = 120.0  # Seconds
 
-#All EC Flags
+# All EC Flags
 ECCheck = True
 ECCheckInterval = 120.0
 
-#Light Cycle Flags
+# Light Cycle Flags
 LighSwitchOnDuration = 64800
 LightSwitchOffDuration = 7200
 
-
+#Thing Speak Credentials
 channel_id = '2271152'
 write_key = 'JO2OUEE6JT9J6OIV'
-
-# Set your credentials JSON file path and Google Sheet name
-CREDENTIALS_FILE = 'automated-hydroponics-data-6667c0d419f8.json'
-SPREADSHEET_NAME = 'Test'
-WORKSHEET_NAME = 'Sheet1'
-
-# Authenticate using the JSON file
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, scope)
-client = gspread.authorize(creds)
-
-# Open the spreadsheet
-spreadsheet = client.open(SPREADSHEET_NAME)
-
-# Select the specific worksheet by name
-worksheet = spreadsheet.worksheet(WORKSHEET_NAME)
 
 ports = list_ports.comports()
 for port in ports:
@@ -78,8 +59,7 @@ time.sleep(1)
 serialCom.flushInput()
 serialCom.setDTR(True)
 
-
-#Some Initializations
+# Some Initializations
 actuation.write((light_switch + ON).encode())
 print("LIGHTS ON")
 LightSwitchDay = True
@@ -96,49 +76,38 @@ received_data = []
 def printSensor():
     print("pH: ", pH)
     print("EC: ", EC, " microSiemens/m")
-    print("Water Temperature: ", round(Water_temperature,2), " Celsius")
-    print("Air Temperature: ", round(Air_temperature,2), " Celsius")
+    print("Water Temperature: ", round(Water_temperature, 2), " Celsius")
+    print("Air Temperature: ", round(Air_temperature, 2), " Celsius")
     print("Humidity: ", Humidity, "%")
     print()
 
 
 while (1):
-    random_value = random.uniform(-0.1,0.1) 
+    random_value = random.uniform(-0.1, 0.1)
     try:
-        
-        # Read a line of data
         current_time = datetime.datetime.now().strftime("%H:%M:%S")
+
+        # Read a line of data
         s_bytes = serialCom.readline()
         decoded_bytes = s_bytes.decode("utf-8").strip("\n\r")
         if counter_n == 1:
             Water_temperature_old = Water_temperature
         lines = decoded_bytes.split(',')
         # Timestamp = float(lines[0])/1000
+
         EC = float(lines[0])
         pH = float(lines[1])
         Humidity = float(lines[2])
-        Air_temperature = 31.5 + random_value
-        Air_temperature = Air_temperature - subtractor_2
-        Water_temperature = 28.2 + random_value
-        Water_temperature = Water_temperature - subtractor
-        counter_n = 1
+        Air_temperature = float(lines[3])
+        Water_temperature = float(lines[4])
         Distance = float(lines[5])
         # print("Current time:", current_time)
-        EC = 403.12
-        pH = 7.6
 
-        if (Water_temperature >50 or Water_temperature < 0):
+
+        if (Water_temperature > 50 or Water_temperature < 0):
             Water_temperature = Water_temperature_old
 
-        if (Water_temperature > 22 and (time.time() - counter >= 15) ):
-            subtractor = subtractor + 0.05
-            counter = time.time()
-
-        if (Air_temperature > 24 and (time.time() - counter_2 >= 40) ):
-            subtractor_2 = subtractor_2 + 0.05
-            counter_2 = time.time()
-
-        if pH < 4 or pH > 14:
+        if pH < 1 or pH > 14:
             pH = 7.0
         printSensor()
 
@@ -146,12 +115,10 @@ while (1):
             start = input("Enter to start Actuation: ")
             counter_start = 1
 
-
-
+        #Thing Speak Code
         send_time = str(current_time)
         data_to_send = [send_time, EC, pH, Humidity, Air_temperature, Water_temperature,
                         Distance]  # Send EC to the Google Sheet
-        worksheet.append_row(data_to_send)
         channel = thingspeak.Channel(id=channel_id, api_key=write_key)
         response = channel.update(
             {'field1': EC, 'field2': pH, 'field3': Humidity, 'field4': Air_temperature, 'field5': Water_temperature,
@@ -159,27 +126,25 @@ while (1):
         printSensor()
 
     except:
-        print(" ")
+        print("Some Error Has Occured!! ")
 
-
-
-#pH Control Unit
+    # pH Control Unit
     if pHCheck == False:
-        if(time.time() - previousPHCheckTime >= pHCheckInterval):
+        if (time.time() - previousPHCheckTime >= pHCheckInterval):
             pHCheck = True
 
     if pHCheck == True:
-        #if too base, add acid
-        if pH > 6.0:
+        # if too base, add acid
+        if pH > 6.5:
             actuation.write((acid_motor + ON).encode())
             print("!!! ACID PUMP IS ON !!!")
-            time.sleep(2)
+            time.sleep(1.5)
             actuation.write((acid_motor + OFF).encode())
             print("!!! ACID PUMP IS OFF !!!")
             previousPHCheckTime = time.time()
             pHCheck = False
 
-        if pH < 5.5:
+        if pH < 5.8:
             actuation.write((base_motor + ON).encode())
             print("!!! BASE PUMP IS ON !!!")
             time.sleep(1)
@@ -189,21 +154,19 @@ while (1):
             pHCheck = False
 
 
-
-    EC = 500
-#EC Control Unit
+    # EC Control Unit
     if ECCheck == False:
-        if(time.time() - previousECCheckTime >= ECCheckInterval):
+        if (time.time() - previousECCheckTime >= ECCheckInterval):
             ECCheck = True
 
     if ECCheck == True:
-        #if EC is too low add both Nutrient A and B
+        # if EC is too low add both Nutrient A and B
         if EC < 1000:
             actuation.write((nutrient_a_motor + ON).encode())
             actuation.write((nutrient_b_motor + ON).encode())
             print("!!! NUTRIENT A PUMP IS ON !!!")
             print("!!! NUTRIENT B PUMP IS ON !!!")
-            time.sleep(2)
+            time.sleep(1)
             actuation.write((nutrient_b_motor + OFF).encode())
             print("!!! NUTRIENT B PUMP IS OFF !!!")
             time.sleep(3)
@@ -212,8 +175,7 @@ while (1):
             previousECCheckTime = time.time()
             ECCheck = False
 
-
-# Day-Night Control Unit (Grow Light Control Unit)
+    # Day-Night Control Unit (Grow Light Control Unit)
     if LightSwitchDay == False:
         if (time.time() - LightSwitchOffTime >= LightSwitchOffDuration):
             actuation.write((light_switch + ON).encode())
@@ -228,18 +190,15 @@ while (1):
             LightSwitchOffTime = time.time()
             LightSwitchDay = False
 
-# Water Cooler Control Unit
+    # Water Cooler Control Unit
     if Water_temperature > 22:
         actuation.write((water_temperature_motor + ON).encode())
-        actuation.write((water_chiller + ON).encode())
         # print("!!! WATER COOLER IS ON !!!")
     if Water_temperature <= 18:
         actuation.write((water_temperature_motor + OFF).encode())
-        actuation.write((water_chiller + OFF).encode())
         # print("!!! WATER COOLER IS OFF !!!")
 
-
-# Air Cooler Control Unit
+    # Air Cooler Control Unit
     if Air_temperature > 24:
         actuation.write((air_temperature_motor + ON).encode())
         # print("!!! AIR COOLER IS ON !!!")
