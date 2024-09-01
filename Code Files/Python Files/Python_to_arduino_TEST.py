c68bae67844a47b0c43a52c8d5c9f7f87b18be17
MD. Sameer Iqbal Chowdhury <sameer.iqbal2171@gmail.com>
2024-09-02 00:47:15 +0600

    Update

--- Code Files/Python Files/Python_to_arduino_TEST.py
+++
@@ -1,9 +0,0 @@
-import serial
-
-arduinoData = serial.Serial("COM9", 9600)
-
-while True:
-    command = input("Enter command: ")
-    command = command + '\r'
-    arduinoData.write(command.encode())
-
